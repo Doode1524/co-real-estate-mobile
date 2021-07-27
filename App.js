93488d9dab0ef4d0bdcfad4a78960e73305948c7
joey <doode1524@gmail.com>
2021-07-27 13:47:15 -0400

    homeimg init

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={styles.homeImg}
+        source={{
+          uri: "https://coloradorealestateus.com/wp-content/uploads/2021/06/colorado-real-estate-main-background-colorado-springs-denver-realtor-find-a-home-11.jpg",
+        }}
+      />
+
+      <Button
+        title="Learn More"
+        accessibilityLabel="Learn more access"
+        onPress={() => Alert.alert("Simple Button pressed")}
+        color="#f194ff"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  homeImg: {
+    width: "100vw",
+    height: "100vh",
   },
 });
+// https://coloradorealestateus.com/wp-content/uploads/2021/06/colorado-real-estate-main-background-colorado-springs-denver-realtor-find-a-home-11.jpg
